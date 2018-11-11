31c151c8ef44696eb449756f89d8fbfa3a50f2fc
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-11 11:19:44 +0200

    refactor: Add UnsupportedMediaType error

--- src/utils/errors/applicationErrors.ts
+++ src/utils/errors/applicationErrors.ts
@@ -1,13 +1,19 @@
-import { ApplicationError } from './applicationError';
+import { ApplicationError, UserError } from './applicationError';
 
-export class RequestValidationError extends ApplicationError {
+export class RequestValidationError extends UserError {
     constructor(message?: string) {
         super(message || 'Request validation failed', 400);
     }
 }
 
-export class ResourceNotFoundError extends ApplicationError {
+export class ResourceNotFoundError extends UserError {
     constructor(resource?: string) {
         super(`${resource || 'Resource'} not found.`, 404);
+    }
+}
+
+export class UnsupportedMediaTypeError extends UserError {
+    constructor(resource?: string) {
+        super(`${resource || 'Resource'} not supported.`, 415);
     }
 }
