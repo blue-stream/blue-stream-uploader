01db18bc2495e8eb1b4bbb2f311f9281e35a1c9d
Ron Borysovski <roni537@gmail.com>
2019-03-11 15:51:14 +0200

    fix: verify token

--- src/upload/upload.validator.ts
+++ src/upload/upload.validator.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { RequestValidationError } from '../utils/errors/applicationErrors';
+import { verify } from 'jsonwebtoken';
+import { config } from '../config';
 
 export function canUpload(req: Request, res: Response, next: NextFunction) {
-    if (!req.query.videoToken) {
-        throw new RequestValidationError();
-    }
+    if (!req.query.videoToken) throw new RequestValidationError();
+
+    const token = verify(req.query.videoToken, config.authentication.secret) as { user: string, video: string };
+    if (!token.video) throw new RequestValidationError();
 
     next();
 }
